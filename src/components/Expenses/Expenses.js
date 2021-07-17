4fcdc0539a653b829630314678a8b8d7cc29ee93
Matt Sirkis <sirkis.matt@gmail.com>
2021-07-17 22:39:45 +1200

    added filter and some more to addExpense form

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,21 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
 import "./Expenses.css";
 
 const Expenses = (props) => {
+  const [expenses, setExpenses] = useState(props.expenses);
+
+  const filterExpensesHandler = (selectedYear) => {
+    if (selectedYear === "all") {
+      setExpenses(props.expenses);
+      return;
+    }
+    const filteredExpense = props.expenses.filter(
+      (expense) => expense.date.getFullYear().toString() === selectedYear
+    );
+    setExpenses(filteredExpense);
+  };
+
   return (
-    <Card className="expenses">
-      {props.expenses.map((expense) => (
-        <div key={expense.id}>
-          <ExpenseItem
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        </div>
-      ))}
-    </Card>
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter onFilterExpensesByYear={filterExpensesHandler} />
+        {expenses.map((expense) => (
+          <div key={expense.id}>
+            <ExpenseItem
+              title={expense.title}
+              amount={expense.amount}
+              date={expense.date}
+            />
+          </div>
+        ))}
+      </Card>
+    </div>
   );
 };
 
